2056433b5427401a3bd69f9396f6bbec6579336c
yonaisatoru <4207572+poindre@users.noreply.github.com>
2024-12-26 11:42:54 +0900

    finished practice

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -21,7 +21,7 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
   end
 
   test 'valid signup information' do
-    assert_fidderence 'User.count', 1 do
+    assert_difference 'User.count', 1 do
       post users_path, params: {
         user: {
           name: "Example User",
@@ -32,6 +32,7 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
       }
     end
     follow_redirect!
-    assssert_template 'users/show'
+    assert_template 'users/show'
+    assert_select 'div.alert-success'
   end
 end
